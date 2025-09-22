28710452b4d4298aa13a7792677581e8af5795b2
al-abd <nexnema@gmail.com>
2025-09-22 17:55:23 +0330

    apps/*

--- packages/lib/src/mergeRefs.ts
+++ packages/lib/src/mergeRefs.ts
@@ -2,7 +2,7 @@ export function mergeRefs<T>(
     ...refs: (React.Ref<T> | undefined | null)[]
 ): React.RefCallback<T> {
     return (value) => {
-        refs.forEach((ref) {
+        refs.forEach((ref) => {
             if (typeof ref === "function") {
                 ref(value);
             } else if (ref && typeof ref === "object" && "current" in ref) {
