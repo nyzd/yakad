dd3430dd19052d7cbf7fac47150d94f4adb9d70e
al-abd <nexnema@gmail.com>
2025-10-09 19:59:44 +0330

    Card hoverEffect

--- packages/ui/src/Card/Card.tsx
+++ packages/ui/src/Card/Card.tsx
@@ -1,33 +1,20 @@
 import { forwardRef } from "react";
 import classNames from "classnames";
-import boxingStyles from "../boxing.module.css";
+import { Stack, StackProps } from "..";
 import styles from "./Card.module.css";
 import "./card.css";
 
-export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
-    align?: "start" | "space" | "center" | "end";
-    fullWidthOnParentDemand?: boolean;
+export interface CardProps extends StackProps {
     blur?: boolean;
     level?: "transparent" | "low" | "mid" | "high";
+    hoverEffect?: boolean;
 }
 
 export const Card = forwardRef<HTMLDivElement, CardProps>(function Card(
-    {
-        align,
-        fullWidthOnParentDemand = true,
-        blur,
-        level,
-        className,
-        children,
-        ...restProps
-    },
+    { blur, level, hoverEffect, className, children, ...restProps },
     ref
 ) {
     const joinedClassNames = classNames(
-        boxingStyles.flexColumnBox,
-        { [boxingStyles[align as string]]: align },
-        { demandChildsToBeFullWidth: true },
-        { fullWidthOnParentDemand: fullWidthOnParentDemand },
         styles.card,
         { [styles.blur]: blur },
         { [styles.elevated]: level !== "transparent" },
@@ -37,12 +24,13 @@ export const Card = forwardRef<HTMLDivElement, CardProps>(function Card(
             yakadCardMidLevel: level === "mid",
             yakadCardHighLevel: level === "high",
         },
+        { [styles.hoverEffect]: hoverEffect },
         className
     );
 
     return (
-        <div ref={ref} {...restProps} className={joinedClassNames}>
+        <Stack ref={ref} {...restProps} className={joinedClassNames}>
             {children}
-        </div>
+        </Stack>
     );
 });
